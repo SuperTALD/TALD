eb4394706eaa0ac0a33d65dacf7d58717a023a94
shpc <pachkakrekerov@gmail.com>
2013-11-19 12:08:08 +0600

    Hello, peoples

---
+++ myTest/src/main/Numbers.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Numbers {
+
+	public int add (int a, int b){
+		return a+b;
+	}
+
+	public int div (int a, int b){
+		return a/b;
+	}
+	
+	public int sub(int a, int b){
+		return a-b;
+	}
+
+}
---
+++ myTest/test/main/TestNumbers.java
@@ -0,0 +1,71 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestNumbers {
+	
+	int a;
+	int b;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		a = 45;
+		b = 15;
+		}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		Numbers num = new Numbers();
+		assertEquals(num.add(a,b), 60);
+	}
+	
+	@Test
+	public void testDivNotNull() {
+		Numbers num = new Numbers();
+		assertNotNull(num.div(a,b));
+	}
+	
+	@Test
+	public void testDiv() {
+		Numbers num = new Numbers();
+		assertEquals(num.div(a,b), 3);
+	}
+	
+	@Test
+	public void testDivFalse() {
+		Numbers num = new Numbers();
+		assertFalse(num.div(a,b) == 4);
+	}
+	
+	@Test
+	public void testSub() {
+		Numbers num = new Numbers();
+		assertEquals(num.sub(a,b), 30);
+	}
+	
+	@Test
+	public void testSubTrue() {
+		Numbers num = new Numbers();
+		assertTrue(num.sub(a, b) == 30);
+	}
+	
+	
+}
